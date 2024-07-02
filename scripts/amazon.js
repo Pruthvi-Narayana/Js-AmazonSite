03688533232f624f25e774c6059df10083ef598c
Pruthvi <pruthvi.robo@gmail.com>
2024-07-02 14:57:40 +0530

    made the Add-to-Cart interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,37 +1,7 @@
-const products = [
-    {
-        image:"images/products/athletic-cotton-socks-6-pairs.jpg",
-        name:"Black and Gray Athletic Cotton Socks - 6 Pairs",
-        rating : {
-            stars : 4.5,
-            count : 87
-        },
-        priceInCents : 1090
-    },
-    {
-        image : "images/products/intermediate-composite-basketball.jpg",
-        name : "Intermediate Size Basketball",
-        rating : {
-            stars : 4,
-            count : 127
-        },
-        priceInCents: 2095
-    },
-    {
-        image:"images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-        name : "Adults Plain Cotton T-Shirt - 2 Pack",
-        rating : {
-            stars : 4.5,
-            coount : 56    
-        },
-        priceInCents : 799
-    }
-]
-
 let productHtml = "";
 products.forEach((product)=>{
     productHtml+= `
-        <div class="product-container">
+        <div class="product-container" loading="lazy">
             <div class="product-image-container">
               <img class="product-image"
                 src="${product.image}">
@@ -75,11 +45,37 @@ products.forEach((product)=>{
               Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart" 
+            data-product-id="${product.id}">
               Add to Cart
             </button>
           </div>
-    `
-})
+    `;
+});
 
-document.querySelector(".js-product-grid").innerHTML = productHtml
+document.querySelector(".js-product-grid").innerHTML = productHtml;
+
+document.querySelectorAll(".js-add-to-cart")
+  .forEach((button)=>{
+    button.addEventListener('click',()=>{
+      const productId= button.dataset.productId;
+
+      let matchingItem; 
+      cart.forEach(currentItem => {
+        if(productId === currentItem.productName){
+          matchingItem = currentItem;
+        }
+      });
+      if (matchingItem){
+        matchingItem.quantity+=1;
+      }
+      else{
+        cart.push({
+          productId,
+          quantity : 1
+        });
+      }
+
+      console.log(cart);
+    });
+});
