bb924eccd8fb08c93ffd2b3d690fac9b138752bd
Pruthvi <pruthvi.robo@gmail.com>
2024-07-03 14:18:07 +0530

    made the Delete in chechout button work.

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,11 @@
-export const cart = [
+export let cart = [
   {
-    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity : 2,
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
   },
   {
-    productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity : 1, 
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
   },
 ];
 
@@ -31,4 +31,17 @@ export function addTocart(productId) {
       quantity: quantity,
     });
   }
+}
+
+export function removeFromCart(productId) {
+   const newCart = []
+
+   cart.forEach(currentItem => {
+     if (currentItem.productId !== productId){
+      newCart.push(currentItem);
+     }
+   });
+
+   cart = newCart;
+   
 }
