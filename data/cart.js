f60065abbc11b1dceab3e40cd1fba9cdcec428ee
Pruthvi <pruthvi.robo@gmail.com>
2024-07-04 12:09:50 +0530

    generated html for delivery options.

--- data/cart.js
+++ data/cart.js
@@ -4,10 +4,12 @@ export let cart = JSON.parse(localStorage.getItem('cart')) ||
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
+    deliverydateId : '1',
   },
   {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 1,
+    deliverydateId : '3',
   },
 ];
 
@@ -35,6 +37,7 @@ export function addTocart(productId) {
     cart.push({
       productId,
       quantity: quantity,
+      deliverydateId : '1'
     });
   }
 
